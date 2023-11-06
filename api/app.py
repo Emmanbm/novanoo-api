3822d6876866f9ae3db737c45310afe346cc0ed7
Emmanbm <emmanboyka@gmail.com>
2023-11-06 23:52:07 +0100

    first commit

---
+++ api/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+# from flask_cors import CORS
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        'message': 'Hello world',
+        'code': 200
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
