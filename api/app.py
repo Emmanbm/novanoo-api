2b5590d2048bd5fa1fb746fa68cd482b99de0550
Emmanbm <emmanboyka@gmail.com>
2023-11-07 00:07:09 +0100

    adding a route test

--- api/app.py
+++ api/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return jsonify({
-        'message': 'Hello world',
+        'message': 'Hello Novanoo !',
         'code': 200
     }), 200
 
